5f3f22d0bf9274ac70c97d57e33ebcd4161932e3
Shrijal Shrestha <61979922+shrijal313@users.noreply.github.com>
2020-07-26 11:08:42 +0545

    added svg

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -10,50 +11,45 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(children: [
-        Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Text("Welcome to CMS", style: GoogleFonts.poppins(
-                textStyle: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.purple[300],
-                  letterSpacing: 1,
-                )
-
-              )),
-            ),
-              Container(
-               color: Colors.red,
-                 width: MediaQuery.of(context).size.width * .9  ,
-                 height:  MediaQuery.of(context).size.height * .6,
-                child: Image(image: AssetImage("asset/images/front.png"),)
-              ),
-              SizedBox(height: 10,),
-              Container(
-                width: MediaQuery.of(context).size.width *.8,
-                margin: EdgeInsets.all(10),
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),
-                color: Color(0xffb780d9)),
-                child: TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  )
-                  
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text("Welcome to CMS",
+                      style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.purple[300],
+                        letterSpacing: 1,
+                      ))),
                 ),
-              
-
-
-            ],
+                SvgPicture.asset(
+                  "asset/images/front.svg",
+                  height: MediaQuery.of(context).size.height * .6,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                    width: MediaQuery.of(context).size.width * .8,
+                    margin: EdgeInsets.all(10),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Color(0xffb780d9)),
+                    child: TextField(
+                      keyboardType: TextInputType.emailAddress,
+                    )),
+              ],
+            ),
           ),
-        ),
-
-
-      ],),
+        ],
+      ),
     );
   }
 }
