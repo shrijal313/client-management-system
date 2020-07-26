165baa99889a9f97344177a6a930280fdfe0f249
Shrijal Shrestha <61979922+shrijal313@users.noreply.github.com>
2020-07-26 11:00:33 +0545

    svg support

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -11,47 +11,45 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(children: [
-        Column(
-          children: [
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Text("Welcome to CMS", style: GoogleFonts.poppins(
-              textStyle: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.w600,
-                color: Colors.purple[300],
-                letterSpacing: 1,
-              )
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text("Welcome to CMS", style: GoogleFonts.poppins(
+                textStyle: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.purple[300],
+                  letterSpacing: 1,
+                )
 
-            )),
-          ),
-            Center(
-              child: Container(
-              //  color: Colors.red,
-                 width: MediaQuery.of(context).size.width * .8,
-                 height:  MediaQuery.of(context).size.height * .4,
+              )),
+            ),
+              Container(
+               color: Colors.red,
+                 width: MediaQuery.of(context).size.width * .9  ,
+                 height:  MediaQuery.of(context).size.height * .6,
                 child: Image(image: AssetImage("asset/images/front.png"),)
               ),
-            ),
-            SizedBox(height: 20,),
-            Container(
-              margin: EdgeInsets.all(10),
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-              decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),
-              color: Color(0xffb780d9)),
-              child: TextField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(fillColor: Colors.red,
-                
-               
-
-                ))
-                
-              ),
-            
+              SizedBox(height: 10,),
+              Container(
+                width: MediaQuery.of(context).size.width *.8,
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),
+                color: Color(0xffb780d9)),
+                child: TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  )
+                  
+                ),
+              
 
 
-          ],
+            ],
+          ),
         ),
 
 
